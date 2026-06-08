aa685db8a5c555d36a3a997ae3f9a2a5db503de3
airelcod <eddymamateunwebo@gmail.com>
2026-06-08 19:53:13 -0400

    Delete api/db directory

--- api/db/logout.js
+++
@@ -1,6 +0,0 @@
-import { sendJSON } from './config.js';
-
-export default async function handler(req, res) {
-    // No hay sesiones que cerrar en JWT, solo informar éxito
-    sendJSON(res, { success: true, message: 'Sesión cerrada' });
-}
