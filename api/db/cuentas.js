cefe53e164ee33ad452712ff4b9fc83bd1006f86
airelcod <eddymamateunwebo@gmail.com>
2026-06-10 12:35:54 -0400

    Update cuentas.js

--- api/db/cuentas.js
+++ api/db/cuentas.js
@@ -1,4 +1,4 @@
-import { query, sendJSON, parseBody } from './config.js';
+import { query, sendJSON, parseBody, transaction } from './config.js';
 
 export default async function handler(req, res) {
     if (req.method === 'OPTIONS') {
