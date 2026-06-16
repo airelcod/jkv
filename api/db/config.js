6d4908668640cb23c578eeeee312458daed73588
airelcod <eddymamateunwebo@gmail.com>
2026-06-16 14:06:46 -0400

    Update config.js

--- api/db/config.js
+++ api/db/config.js
@@ -3,10 +3,10 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
 const dbConfig = {
-    host: process.env.DB_HOST || 'sql3.freesqldatabase.com',
-    user: process.env.DB_USER || 'sql3829733',
-    password: process.env.DB_PASSWORD || '7lYY4HAu23',
-    database: process.env.DB_NAME || 'sql3829733',
+    host: process.env.DB_HOST || 'aws-1-us-east-2.pooler.supabase.com',
+    user: process.env.DB_USER || 'postgres.manhnummghvgumgrivzl',
+    password: process.env.DB_PASSWORD || 'Negroyamarill1(',
+    database: process.env.DB_NAME || 'postgres',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
